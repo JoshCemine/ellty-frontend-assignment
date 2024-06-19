8ffbbde59f987b2bdf855d14bdf49718e1b417ed
Josh Cemine <j.jcemine@gmail.com>
2024-06-20 03:32:51 +0800

    Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+base: "ellty-frontend-assignment"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
